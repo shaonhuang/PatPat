9fb31caedf227391378f812e6fc69a74754ae558
unknown <2586233904@qq.com>
2021-04-18 15:06:24 +0800

    1

--- games/views.py
+++ games/views.py
@@ -18,13 +18,18 @@ def showgame(request):
     kw = request.GET.get('keywords', None)
     ps = int(request.GET.get('pagesize', 10))
     pn = int(request.GET.get('pagenum', 1))
+    lgd= request.GET.get('lastgameid', None)
+
     if ph:
         qs = qs.filter(game_id=ph)
     if kw:
         qs = qs.filter(name__contains = kw)
 
     gameinfo = list(qs)
-    gameinfo=gameinfo[(pn-1)*ps:pn*ps]
+    if lgd:
+        gameinfo = gameinfo[int(lgd):int(lgd)+ps]
+    else:
+        gameinfo=gameinfo[(pn-1)*ps:pn*ps]
     return JsonResponse({'ret': 0, 'gameinfo': gameinfo})
 
 
@@ -34,6 +39,7 @@ def game_comment(request):
     qs = comments.objects.values()
     ps = int(request.GET.get('pagesize', 10))
     pn = int(request.GET.get('pagenum', 1))
+    lci= request.GET.get('lastcommentindex', None)
     # 将 QuerySet 对象 转化为 list 类型
     # 否则不能 被 转化为 JSON 字符串
 
@@ -44,7 +50,10 @@ def game_comment(request):
 
 
         retlist = list(qs)
-        retlist = retlist[(pn - 1) * ps:pn * ps]
+        if lci:
+            retlist = retlist[int(lci):int(lci)+ps]
+        else:
+            retlist = retlist[(pn - 1) * ps:pn * ps]
         return JsonResponse({'ret': 0, 'retlist': retlist})
     else:
         return JsonResponse ({
